d2fece5d06df2b2a5d59b15160eedd7e7d184ed1
Sreejith <132558235+Sreejith-10@users.noreply.github.com>
2024-01-29 13:06:29 +0530

    change

--- src/constants/UserData.ts
+++ src/constants/UserData.ts
@@ -72,7 +72,7 @@ export const skills = [
 export const projectCard: CardData = [
 	{
 		projectId: "git_0",
-		projectImg: "/images/projectImg/blogify (1).png",
+		projectImg: "https://drive.google.com/file/d/1kgQE85PVvToHGEsBx1ZX2-OnwUp9MHbV/view?usp=drive_link",
 		projectTitle: "Blogify Hub",
 		tech: [
 			"ReactJS",
@@ -94,23 +94,16 @@ export const projectCard: CardData = [
 		],
 		github: "",
 		images: [
-			"/images/projectImg/blogify (1).png",
-			"/images/projectImg/blogify (2).png",
-			"/images/projectImg/blogify (3).png",
-			"/images/projectImg/blogify (4).png",
-			"/images/projectImg/blogify (5).png",
-			"/images/projectImg/blogify (6).png",
-			"/images/projectImg/blogify (7).png",
-			"/images/projectImg/blogify (8).png",
-			"/images/projectImg/blogify 3.png",
-			"/images/projectImg/blogify 4",
-			"/images/projectImg/bg-res (1).png",
+			"https://drive.google.com/file/d/1kgQE85PVvToHGEsBx1ZX2-OnwUp9MHbV/view?usp=drive_link",
+			"https://drive.google.com/file/d/1kgOjrw7EgXNcmX69YrIX8rZSoCQ3mK1o/view?usp=drive_link",
+			"https://drive.google.com/file/d/1kdZKppvXDfRUEBG45DDVAs8mwmje9giJ/view?usp=drive_link","https://drive.google.com/file/d/1kcuBPg9aOORGS_0v1l99mC03E7H_zUTR/view?usp=drive_link","https://drive.google.com/file/d/1k_srxwtQfKlua24MuY5BwBIo0zBjKR8Z/view?usp=drive_link"
+			
 		],
 		live: "",
 	},
 	{
 		projectId: "git_1",
-		projectImg: "/images/projectImg/e-com.png",
+		projectImg: "https://drive.google.com/file/d/1kx-HrnPOxOUQuGEOj5iUVOQYJOKjvbl2/view?usp=drive_link",
 		projectTitle: "E-commerce website",
 		tech: ["ReactJs", "JavaScript", "TailwindCSS", "Redux", "Firebase"],
 		description:
@@ -124,24 +117,21 @@ export const projectCard: CardData = [
 			"Sort and filter",
 		],
 		images: [
-			"/images/projectImg/e-com.png",
-			"/images/projectImg/e-com2.png",
-			"/images/projectImg/e-com4.png",
-			"/images/projectImg/e-com5.png",
-			"/images/projectImg/ecom6.png",
+			"https://drive.google.com/file/d/1kx-HrnPOxOUQuGEOj5iUVOQYJOKjvbl2/view?usp=drive_link","https://drive.google.com/file/d/1kuau8WCl7OkIgeAlvlEcu0lJaj1dygCl/view?usp=drive_link","https://drive.google.com/file/d/1kvb4ZakXmetlI1fZ6V-5ksWdFbHs8fTw/view?usp=drive_link",
+			"https://drive.google.com/file/d/1kv9QI-E7s8gZp6a7G2xmOfd_QuVRQhjO/view?usp=drive_link","https://drive.google.com/file/d/1ktBmAkiivPuwnu3h1FIWryXiSc2Ov7Q9/view?usp=drive_link"
 		],
 		github: "https://github.com/Sreejith-10/E-commerce",
 		live: "https://e-commerce-app-54e4a.web.app/",
 	},
 	{
 		projectId: "git_2",
-		projectImg: "/images/projectImg/chat1.png",
+		projectImg: "",
 		projectTitle: "Chat Zone",
 		tech: ["ReactJS", "JavaScript", "Tailwind", "Firebase"],
 		description:
 			"Chat zone is an web application made with react and firebase which users can add friends and message them.",
 		features: ["Real time chat", "Search friends", "Upload images"],
-		images: ["/images/projectImg/chat1.png"],
+		images: [],
 		github: "https://github.com/Sreejith-10/Chat-app",
 		live: "https://chat-app-167f4.firebaseapp.com/",
 	},
@@ -157,23 +147,20 @@ export const projectCard: CardData = [
 			"get all ingredients and cooking instruction",
 		],
 		images: [
-			"/images/projectImg/food.png",
-			"/images/projectImg/food1.png",
-			"/images/projectImg/food2.png",
-			"/images/projectImg/food3.png",
+		
 		],
 		github: "https://github.com/Sreejith-10/Food-Recipe-App",
 		live: "https://sreejith-10.github.io/Food-Recipe-App/",
 	},
 	{
 		projectId: "git_4",
-		projectImg: "/images/projectImg/todo.png",
+		projectImg: "",
 		projectTitle: "Todo App",
 		tech: ["ReactJS", "HTML", "CSS"],
 		description:
 			"This is a simple todo app made with react js with crud operations",
 		features: ["Basic Crud operations like add task,edit,delete"],
-		images: ["/images/projectImg/todo.png"],
+		images: [],
 		github: "https://github.com/Sreejith-10/TodoApp-React",
 		live: "https://sreejith-10.github.io/TodoApp-React/",
 	},
@@ -185,7 +172,7 @@ export const projectCard: CardData = [
 		description:
 			"A fun tic-tac-toe game made for having a fun time with friends.The players can see live scores and winners",
 		features: ["Score board"],
-		images: ["/images/projectImg/tic.png"],
+		images: [],
 		github: "https://github.com/Sreejith-10/Tic-Tac-Toe",
 		live: "https://sreejith-10.github.io/Tic-Tac-Toe/",
 	},
